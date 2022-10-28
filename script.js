03745e44bd699687098ac717164721d5d8fb5d42
Raghu-KV <raghunandanv19@gmail.com>
2022-10-28 17:13:55 +0530

    updated serch

--- script.js
+++ script.js
@@ -2,6 +2,21 @@
 let container = document.createElement("div")
 container.classList.add("container")
 
+
+let section = document.createElement("select")
+section.classList.add("form-control" , "position-my")
+section.setAttribute("id","serch")
+
+
+let bookNames = ["All Books", "A Game of Thrones","A Clash of Kings","A Storm of Swords","The Hedge Knight","A Feast for Crows", "The Sworn Sword", "The Mystery Knight", "A Dance with Dragons", "The Princess and the Queen" , "The Rogue Prince"]
+
+for(let book of bookNames){
+    let opt = document.createElement("option")
+    opt.innerText = book
+    section.append(opt)
+}
+
+
 let row = document.createElement("div")
 row.classList.add("row")
 
@@ -9,11 +24,18 @@ document.body.append(container)
 
 let para = document.createElement("p")
 
-para.classList.add("text-center" ,"text-white")
-para.innerText="Ice and Fire is a series of epic fantasy novels by the American novelist and screenwriter George R. R. Martin. He began the first volume of the series, A Game of Thrones, in 1991, and it was published in 1996. Martin, who initially envisioned the series as a trilogy, has published five out of a planned seven volumes. The most recent volume of the series, A Dance with Dragons,"
+para.classList.add("text-center")
+para.innerText = "Ice and Fire is a series of epic fantasy novels by the American novelist and screenwriter George R. R. Martin. He began the first volume of the series, A Game of Thrones, in 1991, and it was published in 1996. Martin, who initially envisioned the series as a trilogy, has published five out of a planned seven volumes. The most recent volume of the series, A Dance with Dragons,"
+
+let h4 = document.createElement("h4")
+h4.classList.add("text-center")
+h4.innerText="Serch for books"
+
+
 
 container.append(para)
-
+container.append(h4)
+container.append(section)
 container.append(row)
 
 // fetching the API using async function
@@ -26,46 +48,69 @@ async function iceNfire() {
         // the API data is stored in dataOfBooks
         for (let i = 0; i < dataOfBooks.length; i++) {
             row.innerHTML += `
-                <div class="col-sm-12 col-md-6 col-lg-4 text-center mt-2 mb-4  d-flex justify-content-center">
-                <div class="card" style="width: 18rem;">
-                <img src="images/${[i]}.jpg" class="card-img-top" alt="..." style ="height: 350px">
-                <div class="card-body">
-                <h4 class="card-title text-primary">${dataOfBooks[i].name}</h4>
-                <ul class="list-group list-group-flush">
-                <li class="list-group-item text-secondary">ISBN : ${dataOfBooks[i].isbn}</li>
-                <li class="list-group-item text-secondary">No. of Pg : ${dataOfBooks[i].numberOfPages}</li>
-                <li class="list-group-item text-secondary">Author : ${dataOfBooks[i].authors[0]}</li>
-                <li class="list-group-item text-secondary">Publisher : ${dataOfBooks[i].publisher}</li>
-                <li class="list-group-item text-secondary">Released : ${dataOfBooks[i].released.slice(0,10)}</li>
-                </ul>
-                <a href="#" class="btn btn-primary mt-3">Show Characters</a>
-                </div>
-                </div>
+                <div class="col-sm-12 col-md-12 col-lg-6 text-center mt-2 mb-4 select">
+                <div class="card mb-3" style="min-width: 240px;">
+  <div class="row no-gutters">
+    <div class="col-4">
+      <img src="images/${i}.jpg" alt="..." style="width: 100%; height: 250px">
+    </div>
+    <div class="col-8">
+      <div class="card-body p-3">
+        <h5 class="card-title">${dataOfBooks[i].name}</h5>
+        <p class="card-text text-muted mb-1"><small>ISBN : ${dataOfBooks[i].isbn}</small></p>
+        <p class="card-text text-muted mb-1"><small>No. of Pg : ${dataOfBooks[i].numberOfPages}</small></p>
+        <p class="card-text text-muted mb-1"><small>Publisher : ${dataOfBooks[i].publisher}</small></p>
+        <p class="card-text text-muted mb-1"><small> released date : ${dataOfBooks[i].released.slice(0,10)}</small></p>
+        <p class="card-text text-muted mb-1"><small>author : ${dataOfBooks[i].authors[0]}</small></p>
+        <button type="button" class="btn btn-outline-dark mt-2">Show Characters</button>
+      </div>
+    </div>
+  </div>
+</div>
                 </div>`
         }
 
         let btn = document.querySelectorAll(".btn")
-        
+
         for (let i = 0; i < btn.length; i++) {
-        
-            btn[i].addEventListener("click", async (event)=>{
-                event.preventDefault()
-                let arr = []
-                
-                for (let j = 10; j < 15; j++){
-                    let char = await fetch(dataOfBooks[i].characters[j])
-                    let char5 = await char.json()
-                    arr.push(char5.name)
-                }
-                alert(`                1st Character : ${arr[0]}
+            try {
+                btn[i].addEventListener("click", async (event) => {
+                    event.preventDefault()
+                    let arr = []
+
+                    for (let j = 10; j < 15; j++) {
+                        let char = await fetch(dataOfBooks[i].characters[j])
+                        let char5 = await char.json()
+                        arr.push(char5.name)
+                    }
+                    alert(`                1st Character : ${arr[0]}
                 2nd Character : ${arr[1]}
                 3rd Character : ${arr[2]}
                 4th Character : ${arr[3]}
                 5th Character : ${arr[4]}`)
-                arr = []
-            })  
+                    arr = []
+                })
+            } catch (err) { console.log(err)}
         }
-    // catch block to console th error    
+
+        let cards = document.querySelectorAll(".select")
+        
+
+        let inpt = document.querySelector("#serch")
+    
+        inpt.addEventListener("input",(e)=>{
+            
+            for(let card of cards){
+               if(inpt.value === "All Books"){
+                card.classList.remove("hide")
+               } else{
+                let lower = card.innerText.toLowerCase()
+                let isVisible = lower.includes(inpt.value.toLowerCase())
+                card.classList.toggle("hide", !isVisible)
+               }
+            }
+        })
+        // catch block to console th error    
     } catch (err) {
         console.log(err)
     }
